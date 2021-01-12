b5dae986406a0be4466bfaab9a6aa60c771aff56
Chase Wickett <wickettc@yahoo.com>
2021-01-11 19:54:43 -0500

    still working on hourly.js

--- src/pages/Hourly.js
+++ src/pages/Hourly.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import ListData from '../components/ListData';
 
 const Hourly = (props) => {
-    const [initDate, setInitDate] = useState('');
+    const [displayDate, setDisplayDate] = useState(
+        new Date(props.hourly[0].dt * 1000).toLocaleString('en-US', {
+            weekday: 'long',
+            day: 'numeric',
+        })
+    );
 
     const renderHourly = props.hourly.map((hour) => {
         const date = new Date(hour.dt * 1000).toLocaleString('en-US', {
@@ -10,9 +15,13 @@ const Hourly = (props) => {
             day: 'numeric',
         });
 
+        if (displayDate !== date) {
+            setDisplayDate(date);
+        }
+
         return (
             <div>
-                {date !== initDate ? <div>{date}</div> : null}
+                {displayDate !== date ? date : null}
                 <ListData key={hour.dt} hour={hour} />
             </div>
         );
